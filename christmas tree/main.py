146afd5050cd31f713866fa4d22652340e78b14a
mohawk2846 <super_aleks9@ukr.net>
2022-12-25 21:32:23 +0200

    work

---
+++ christmas tree/main.py
@@ -0,0 +1,62 @@
+# import turtle
+#
+# a = turtle.Turtle()
+# turtle.pen(fillcolor="green")
+# turtle.begin_fill()
+# turtle.speed(4)
+# turtle.forward(80)
+# turtle.left(120)
+# turtle.forward(80)
+# turtle.left(120)
+# turtle.forward(80)
+# turtle.end_fill()
+#
+# turtle.left(120)
+# turtle.forward(40)
+#
+# turtle.pen(fillcolor="green")
+# turtle.begin_fill()
+# turtle.right(60)
+# turtle.forward(100)
+# turtle.right(120)
+# turtle.forward(100)
+# turtle.right(120)
+# turtle.forward(100)
+# turtle.backward(100)
+# turtle.end_fill()
+#
+# turtle.right(60)
+# turtle.forward(50)
+#
+# turtle.pen(fillcolor="green")
+# turtle.begin_fill()
+# turtle.right(120)
+# turtle.forward(120)
+# turtle.left(120)
+# turtle.forward(120)
+# turtle.left(120)
+# turtle.forward(120)
+# turtle.end_fill()
+#
+# turtle.backward(120)
+# turtle.left(60)
+#
+# turtle.pen(fillcolor="brown")
+# turtle.begin_fill()
+# turtle.forward(45)
+# turtle.left(90)
+# turtle.forward(30)
+# turtle.right(90)
+# turtle.forward(30)
+# turtle.right(90)
+# turtle.forward(30)
+# turtle.end_fill()
+#
+# turtle.done()
+
+
+
+
+
+
+
